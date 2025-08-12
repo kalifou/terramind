5a1f89ca5170144911fb67b8d3fbe5e33e22ea12
kalifou <renetrao@gmail.com>
2025-08-12 10:19:57 +0200

    hpc scripts: fix scripts for benchmarking tim.

--- scripts/tim/compute_node__terrabyte.sh
+++ scripts/tim/compute_node__terrabyte.sh
@@ -7,18 +7,20 @@ conda activate gruenblick_terramind
 list_tim_sensor_i=$1
 backbone_size=base
 
-max_epoch_i=3
+max_epoch_i=50
 batch_size=32
 dataset=/dss/dsshome1/04/di38jul/swattainer/temporary_rene/biomassters/
 log_dir=../../results/benchmkark_tim/
 
+echo "list of backbones: "$list_tim_sensor_i
+
 python ../../benchmarking_terramind_on_biomassters.py \
--tst \
--fzb \      
--sr s2 \
--ltim $list_tim_sensor_i\
--bcsz  $backbone_size \
--me $max_epoch_i \
--bs $batch_size \
--pthd $dataset \
--pthl  $log_dir >> tim_$list_tim_sensor_i"__frozen__"$sensor_i$(date +"__%Y_%m_%d__%H_%M_%S")"_.out" 
+       -sd 1\
+       -fzb \
+       -sr s2 \
+       -ltim $list_tim_sensor_i\
+       -bcsz  $backbone_size \
+       -me $max_epoch_i \
+       -bs $batch_size \
+       -pthd $dataset \
+       -pthl  $log_dir >> tim_$list_tim_sensor_i"__frozen__"sen2_$(date +"__%Y_%m_%d__%H_%M_%S")"_.out" 
--- scripts/tim/dispatch_to_nodes__terrabyte.sh
+++ scripts/tim/dispatch_to_nodes__terrabyte.sh
@@ -4,17 +4,17 @@
 
 # Single TiM modality
 
-for tim_sensor_i in sen1, dem, ndvi, lulc; do
+for tim_sensor_i in sen1 dem ndvi lulc; do
     sbatch login_node__terrabyte.sh $tim_sensor_i
 done
 
 
-for tim_sensor_i in dem, ndvi, lulc; do
-    sbatch login_node__terrabyte.sh sen1","$tim_sensor_i
-done
+#for tim_sensor_i in dem ndvi lulc; do
+#    sbatch login_node__terrabyte.sh sen1","$tim_sensor_i
+#done
 
-for tim_sensor_i_inner in sen1, dem, ndvi, lulc; do
-    for tim_sensor_outter in sen1, dem, ndvi, lulc; do
+for tim_sensor_i_inner in sen1 dem ndvi lulc; do
+    for tim_sensor_outter in sen1 dem ndvi lulc; do
         if [ $tim_sensor_i_inner != $tim_sensor_outter ]; then 
             sbatch login_node__terrabyte.sh $tim_sensor_i_inner","$tim_sensor_outter;
         fi
