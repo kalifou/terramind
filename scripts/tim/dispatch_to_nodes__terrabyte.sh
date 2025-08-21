9091d24e41083ea12496f7e32e44e10816375c4a
kalifou <renetrao@gmail.com>
2025-08-21 11:04:03 +0200

    tim benchmarking: increase the chain of modalities.

--- scripts/tim/dispatch_to_nodes__terrabyte.sh
+++ scripts/tim/dispatch_to_nodes__terrabyte.sh
@@ -7,23 +7,37 @@ for funetuning_sensor_i in s1 s2; do
     for tim_sensor_i in sen1 dem ndvi lulc; do
         sbatch login_node__terrabyte.sh $tim_sensor_i $funetuning_sensor_i
     done
-done
+    
+     if [ funetuning_sensor_i == s1 ]; then 
+         tim_sentinel_modality=sen2
+     else
+         tim_sentinel_modality=sen1
+     fi
 
-
-funetuning_sensor_i=s2
-for tim_sensor_i_inner in sen1 dem ndvi lulc; do
-    for tim_sensor_outter in sen1 dem ndvi lulc; do
-        if [ $tim_sensor_i_inner != $tim_sensor_outter ]; then 
-            sbatch login_node__terrabyte.sh $tim_sensor_i_inner","$tim_sensor_outter $funetuning_sensor_i
-        fi
+    for tim_sensor_i_inner_1 in $tim_sentinel_modality dem ndvi lulc; do
+        
+        sbatch login_node__terrabyte.sh $tim_sensor_i_inner_1 $funetuning_sensor_i
+    
+        for tim_sensor_i_inner_2 in $tim_sentinel_modality dem ndvi lulc; do
+            
+            # TIM with a list of 2 modalities
+            if [ $tim_sensor_i_inner_1 != $tim_sensor_i_inner_2 ] ; then 
+                sbatch login_node__terrabyte.sh $tim_sensor_i_inner_1","$tim_sensor_i_inner_2 $funetuning_sensor_i
+            fi
+            
+            # TIM with a list of 3 modalities
+            for tim_sensor_i_outer in $tim_sentinel_modality dem ndvi lulc; do
+            
+                # Making sure of the permutations of 3 modalities is free from redudant sensors (each much be unique)
+                if [ $tim_sensor_i_inner_1 != $tim_sensor_i_inner_2 ] &&  [ $tim_sensor_i_inner_1 != $tim_sensor_i_outer ] && [ $tim_sensor_i_inner_2 != $tim_sensor_i_outer ]; then 
+                    sbatch login_node__terrabyte.sh $tim_sensor_i_inner_1","$tim_sensor_i_inner_2","$tim_sensor_i_outer $funetuning_sensor_i
+                fi
+                
+            done
+        done
     done
+
+
 done
 
-funetuning_sensor_i=s1
-for tim_sensor_i_inner in sen2 dem ndvi lulc; do
-    for tim_sensor_outter in sen2 dem ndvi lulc; do
-        if [ $tim_sensor_i_inner != $tim_sensor_outter ]; then 
-            sbatch login_node__terrabyte.sh $tim_sensor_i_inner","$tim_sensor_outter $funetuning_sensor_i
-        fi
-    done
-done
+
